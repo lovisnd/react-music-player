0c7313910addaca3a3a62a0749a6ff346c716673
lovisnd <39151286+lovisnd@users.noreply.github.com>
2019-10-16 22:57:28 +0800

    Update musiclist.js

--- app/config/musiclist.js
+++ app/config/musiclist.js
@@ -1,40 +1,58 @@
 export const MUSIC_LIST= [
     {
         id: 1,
-        title: '天使中的魔鬼',
-        artist: '田馥甄',
-        file: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3',
-        cover: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.jpg'
+        title: '月光探戈',
+        artist: '吴佳怡',
+        file: 'https://sharefs.yun.kugou.com/201910162112/0c484088e1b3cee25bc2f9c9343dd51c/G168/M07/16/07/iJQEAF1KiSCAGyUKADf3J3fs4TQ414.mp3',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000001Euw372UTbRE.jpg?max_age=2592000'
     }, {
         id: 2,
-        title: '风继续吹',
-        artist: '张国荣',
-        file: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%A3%8E%E7%BB%A7%E7%BB%AD%E5%90%B9.mp3',
-        cover: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%A3%8E%E7%BB%A7%E7%BB%AD%E5%90%B9.jpg'
+        title: '小城谣',
+        artist: '封茗囧菌&洛少爷',
+        file: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_46751277&response=res&type=convert_url&',
+        cover: 'http://imge.kugou.com/stdmusic/240/20161029/20161029032539128398.jpg'
     }, {
         id: 3,
-        title: '恋恋风尘',
-        artist: '老狼',
-        file: 'http://oj4t8z2d5.bkt.clouddn.com/%E6%81%8B%E6%81%8B%E9%A3%8E%E5%B0%98.mp3',
-        cover: 'http://oj4t8z2d5.bkt.clouddn.com/%E6%81%8B%E6%81%8B%E9%A3%8E%E5%B0%98.jpg'
+        title: '戏语花',
+        artist: '玄觞',
+        file: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_23657372&response=res&type=convert_url&',
+        cover: 'http://imge.kugou.com/stdmusic/240/20161030/20161030233237340780.jpg'
     }, {
         id: 4,
-        title: '我要你',
-        artist: '任素汐',
-        file: 'http://oj4t8z2d5.bkt.clouddn.com/%E6%88%91%E8%A6%81%E4%BD%A0.mp3',
-        cover: 'http://oj4t8z2d5.bkt.clouddn.com/%E6%88%91%E8%A6%81%E4%BD%A0.jpg'
+        title: '辞九门回忆',
+        artist: '妙语清歌',
+        file: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_7040602&response=res&type=convert_url&',
+        cover: 'http://imge.kugou.com/stdmusic/240/20190226/20190226122908365495.jpg'
     }, {
         id: 5,
-        title: '成都',
-        artist: '赵雷',
-        file: 'http://oj4t8z2d5.bkt.clouddn.com/%E6%88%90%E9%83%BD.mp3',
-        cover: 'http://oj4t8z2d5.bkt.clouddn.com/%E6%88%90%E9%83%BD.jpg'
+        title: '典狱司',
+        artist: '音频怪物',
+        file: 'http://ws.stream.qqmusic.qq.com/C400003OuAaw1NT7eA.m4a?guid=628488616&vkey=06BFA079140219A6504444C0E0BF4C51151DE8FB3225D06CD7C2604A7B8400CAA320A1B18CED076B49A6D998E97FC095198390E8F295F4CB&uin=0&fromtag=66',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000001sKiwt2SUl5z.jpg?max_age=2592000'
     }, {
         id: 6,
-        title: 'sound of silence',
-        artist: 'Simon & Garfunkel',
-        file: 'http://oj4t8z2d5.bkt.clouddn.com/sound-of-silence.mp3',
-        cover: 'http://oj4t8z2d5.bkt.clouddn.com/sound-of-silence.jpg'
+        title: '一生独一',
+        artist: '卢焱',
+        file: 'http://ws.stream.qqmusic.qq.com/C400003gcCuO4GunG6.m4a?guid=217979596&vkey=CC9C6968B4398FF6A6FB44043AA04EF72B7D757025B7D6C4F3B69EF0FC60C651BABBD29988EC6F516F52628361C857F73217DE9770863E55&uin=0&fromtag=66',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000003gG4ns1MSti2.jpg?max_age=2592000'
+    }, {
+        id: 7,
+        title: '牵丝戏',
+        artist: '银临 / Aki阿杰',
+        file: 'http://183.252.54.25/amobile.music.tc.qq.com/C400001ptehR2XQlgN.m4a?guid=417583850&vkey=96486F6C5EAB89BEF59E1D98804EA3F995565A8679C76EF72F8342CD7A797D11E318B8A8FC9F0025D6A21CADDB967950A332891BF2B55A4C&uin=0&fromtag=66',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000000uGdjk1J7qA2.jpg?max_age=2592000'
+    }, {
+        id: 8,
+        title: '琵芭行',
+        artist: '奇然,沈谧仁',
+        file: 'http://183.252.54.15/amobile.music.tc.qq.com/C4000032QZRv3VfiFX.m4a?guid=723183799&vkey=5B4F70F63689C0DFE948DD6D794F2FB519B028D7AA279B41E54DD4AAD1D0A22251341706343458758489E485A2D0DE2A02231779EDC2884B&uin=0&fromtag=66',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000003OSjGL1uNVGw.jpg?max_age=2592000'
+    }, {
+        id: 9,
+        title: '如果当时',
+        artist: '许嵩',
+        file: 'http://ws.stream.qqmusic.qq.com/C400003uh6Y81FI4Nv.m4a?guid=523595739&vkey=660AE14EBD4870C7C93CAFF4F267D7830E4219BDA3A8406C2933F27CFD8484D8316C4FDBA3C0A2093ED33DB7A45F5AEC3938757FAEE9BF16&uin=0&fromtag=66',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000002KSDg90IaScI.jpg?max_age=2592000'
     }
 
 ];
